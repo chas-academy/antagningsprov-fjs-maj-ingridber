763569fbc3e9594faecef54dcbac7daa9ae14dae
Ingrid Berggren <155575012+ingridber@users.noreply.github.com>
2025-05-30 10:21:01 +0200

    Update uppg6.js

--- uppg6.js
+++ uppg6.js
@@ -5,7 +5,26 @@ function uppg6(){
     // Skapa en array med namn som du döper till names
     // Loopa igenom arrayen och skriv ut alla namn i konsolen
 
- 
+     let names = ["John", "George", "Ringo", "Paul"]
+
+    for (let name of names){
+        console.log(name)
+    }
+
+
+    /*
+    --- ALTERNATIVT ---
+
+    function uppg6b(){
+
+        let names = ["Björn", "Benny", "Anni-Frid", "Agnetha"]
+
+        names.forEach(name => {
+            console.log(name)
+        })
+    }
+    */
+    
 }
 
 module.exports = { uppg6 };
