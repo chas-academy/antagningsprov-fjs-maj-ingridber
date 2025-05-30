fe6d171b994c3f2b454eb348d03ee412d9537a7f
Ingrid Berggren <155575012+ingridber@users.noreply.github.com>
2025-05-30 10:33:20 +0200

    Update uppg7.js

--- uppg7.js
+++ uppg7.js
@@ -6,7 +6,12 @@ function uppg7() {
     // funktionen ska returnera summan av talen
     // kalla på funktionen i en return statement och ge den parametrarna 5 och 10
 
- 
+   function sum(x, y) {
+      return x + y
+   }
+
+   console.log(sum(5,10))
+   
 }
 
 module.exports = { uppg7 };
