9c7441080af49c8c562a27e75bb753993c791fe8
Ingrid Berggren <155575012+ingridber@users.noreply.github.com>
2025-05-30 09:53:01 +0200

    Update uppg3.js

--- uppg3.js
+++ uppg3.js
@@ -7,9 +7,21 @@ function uppg3() {
     // tilldela värdena 5 och 10 till variablerna
     // skriv ut summan av a och b i konsolen
 
-
-
+    let a = 5
+    let b = 10
+    sum = a + b 
+    console.log(sum)
 }
+
+
+// alternativt
+
+function uppg3b(a, b) {
+    sum = a + b
+    console.log(sum)
+}
+
+uppg3b(5,10)
 
 
 module.exports = { uppg3 };
